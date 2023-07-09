dd08df14c3a0bae74271cf5fedd40ad7e2457406
chae1xx1os <kty1895@naver.com>
2023-07-09 18:28:27 +0900

    [7-2] Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  const effectFn = () => {
-    console.log('Created')
-    return destroyFn
-  }
-  const destroyFn = () => console.log('Destroyed');
-  
-  useEffect(effectFn, [])
-  
-  return <h1>Hello</h1>
-}
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClic = {onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
-
-export default App;
